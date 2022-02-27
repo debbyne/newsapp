bda2b32979def2598d8b15779e44f0a28a7c509d
debbyne <kinyadebbyne@gmail.com>
2022-02-27 17:36:27 +0300

    add articles class

--- models.py
+++ models.py
@@ -8,3 +8,14 @@ class Source:
         self.source_name=source_name
         self.source_url=source_name
         
+class articles:
+    '''
+    class that defines articles
+    '''
+    def __init__(self,article_image_url,article_source,article_title,article_author,article_description,time_published):
+        self.article_image_url=article_image_url
+        self.article_source=article_source
+        self.article_title=article_title
+        self.article_author=article_author
+        self.article_description=article_description
+        self.time_published=time_published
